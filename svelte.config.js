49d1843e0530e5db995993450f1899866626176c
Neeraj Vernekar <neeraj.v@outlook.com>
2026-02-22 15:17:50 -0500

    add cloudflare page support

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,15 +7,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: process.env.CF_PAGES
+			? adapterCloudflare()
+			: adapterStatic({
+					// default options are shown. On some platforms
+					// these options are set automatically — see below
+					pages: 'build',
+					assets: 'build',
+					fallback: '404.html',
+					precompress: false,
+					strict: true
+				})
 	}
 };
 
